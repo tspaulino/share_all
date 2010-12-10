524055844780c9afc5b013c540cdaceeedf4973d
Tiago Paulino <tspaulino@gmail.com>
2010-12-10 00:12:31 -0200

    Fix for paperclip path

--- config/environment.rb
+++ config/environment.rb
@@ -40,4 +40,4 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
-Paperclip.options[:command_path] = "/opt/local/bin"
+#Paperclip.options[:command_path] = "/opt/local/bin"
